07499b9e5dd4dbf841c1cf95267b6bdf853a8bd5
KotSnozom <166803529+TheCodCat@users.noreply.github.com>
2024-09-19 17:56:14 +0400

    aaaa

--- Assets/Scripts/ScheduleMode/ScheduleController.cs
+++ Assets/Scripts/ScheduleMode/ScheduleController.cs
@@ -57,7 +57,7 @@ public class ScheduleController : MonoBehaviour
 
     public async Task GetGroupCabsPositonAsync()
     {
-        var _listPars = await _api.Schedule.GetScheduleAsync(new DateOnlyLegacy(_time.Year, _time.Month, _time.Day - 2), _currentGroup);
+        var _listPars = await _api.Schedule.GetScheduleAsync(new DateOnlyLegacy(_time.Year, _time.Month, _time.Day), _currentGroup);
 
         Vector3[] post = new Vector3[_listPars.Lessons.Count];
 
